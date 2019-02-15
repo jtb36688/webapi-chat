ae1044d5dc263be4eb86089902fb94a381770b68
Jacob Bryan <jtb36688@gmail.com>
2019-02-14 19:06:46 -0500

    JB Api-Chat

---
+++ data/dbConfig.js
@@ -0,0 +1,5 @@
+const knex = require("knex");
+const db = process.env.DB_ENV || "development";
+const config = require("../knexfile.js")[db];
+
+module.exports = knex(config);
---
+++ server.js
@@ -0,0 +1,69 @@
+const express = require('express'); // importing a CommonJS module
+const helmet = require('helmet')
+const morgan = require('morgan')
+const config = require('./data/dbConfig.js')
+
+const hubsRouter = require('./hubs/hubs-router.js');
+
+const server = express();
+
+function teamNamer(req, res, next) {
+  req.team = 'Web 16'
+  next();
+}
+
+// global middleware pipeline/chain
+server.use(express.json());
+server.use(helmet());
+server.use(morgan());
+server.use(teamNamer);
+
+server.use('/api/hubs', hubsRouter);
+
+
+server.get('/', async (req, res, next) => {
+  if(req.headers.name === 'po') {
+  res.send(`
+    <h2>Lambda Hubs API</h2>
+    <p>Welcome ${req.team} to the Lambda Hubs API</p>
+  `);
+  } else {
+    next('any argument will trigger the next error handling middleware')
+  }
+});
+
+server.use(errorHandler)
+
+
+// implementation
+
+function errorHandler(err, req, res, next) {
+  res.status(400).json({ message: "bad trash panda" })
+}
+
+// authorization is a foobar banana property
+function restricted(req, res, next) {
+  const password = req.headers.authorization
+
+  if (password === 'mellow') {
+    next();
+  } else {
+    res.status(401)
+  }
+}
+
+function moodyGateKeeper(req, res, next) {
+  const seconds = new Date().getSeconds();
+
+  if (seconds % 3 === 0) {
+    res.status(403).json({ you: 'shall not pass!'})
+  } else {
+    next();
+  }
+}
+
+server.use('/api/hubs', hubsRouter);
+// server.use(moodyGateKeeper)
+
+// export default server; ES2015 Modules
+module.exports = server;
